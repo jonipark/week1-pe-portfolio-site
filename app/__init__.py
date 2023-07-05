e44cdeb5229e71116be7e86412091199bf72ff19
Nimra <56455442+nimra200@users.noreply.github.com>
2023-07-05 14:53:39 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Prairie Dogs - Nimra's portfolio", url=os.getenv("URL"))
+    return render_template('index.html', title="Prairie Dogs - Nimra & Joni's portfolio", url=os.getenv("URL"))
 
 
 @app.route('/hobby')
